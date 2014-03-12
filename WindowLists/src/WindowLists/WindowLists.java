98017a748cd362b04fcbc2260bb7be4d0121462b
dludaya <dludaya.cs@gmail.com>
2014-03-12 00:07:35 -0400

    update

--- WindowLists/src/WindowLists/WindowLists.java
+++ WindowLists/src/WindowLists/WindowLists.java
@@ -12,27 +12,17 @@ public class WindowLists {
 		l.add(4);
 		l.add(1);
 		int window=2;
-		int[] s=new int[l.size()];
+		int[] s=new int[l.size()-window+1];
+		int temp;
 		for(int i=0;i<window;i++){
 			s[0]=s[0]+l.get(i);
 		}
+		temp=s[0];
 		int j=0, k=1, sum=0;
-		for(int i=0;i<l.size(); i++){
-			System.out.println("i="+i);
-			if(j<=window){
-				sum=sum+l.get(i);
-				System.out.println("Sum="+sum);
-				j++;
-			}
-			else{
-				s[k]=sum-l.get(k-1);
-				System.out.println("s[k]="+s[k]);
-				i=k-1;
-				k++;
-				j=0;
-				sum=0;
-			}
-			
+		for(int i=1; i<l.size()-window+1; i++)
+		{
+			s[i]=temp+l.get(i+window-1)-l.get(i-1);
+			temp=s[i];
 		}
 		for(int i=0;i<s.length; i++){
 			System.out.println(s[i]);
